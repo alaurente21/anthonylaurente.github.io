663e6bccb3f04cccb0b1f2d85e5b72118b74ae74
alaurente21 <74069975+alaurente21@users.noreply.github.com>
2022-04-30 21:58:45 -0700

    Update fancifytext with functions

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,6 +1,39 @@
 /* Anthony Laurente CSC196W */
 
-// Alert
+/*
 function alert() {
   alert("Hello World!");
+}
+*/
+
+// Bigger function
+function Big() {
+  var text = document.getElementById("textinput");
+  text.style.fontSize = "30pt";
+}
+
+// Bold function 
+function Bold() {
+  var text = document.getElementById("textinput");
+  text.style.fontWeight = "bold";
+  text.style.color = "red";
+  text.style.textDecoration = "underline";
+}
+
+// BoringBetty Function
+function Boring() {
+  var text = document.getElementById("textinput");
+  text.style.fontSize = "initial";
+  text.style.fontWeight = "normal";
+  text.style.color = "black";
+  text.style.textDecoration = "none";
+}
+
+
+// Moo function
+function Moo() {
+  var suffix = '-Moo',
+  txtInput = document.getElementById("textinput");
+  txtInput.style.textTransform = "uppercase";
+  txtInput.value = txt.value.split('.').join('') + suffix;
 }
